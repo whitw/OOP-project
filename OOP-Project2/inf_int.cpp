482187379839cff5f5a7c9c7a30bd8f411053e1e
whitw <shkim7876@naver.com>
2019-10-07 17:19:02 +0900

    cin >> inf_int

--- OOP-Project2/inf_int.cpp
+++ OOP-Project2/inf_int.cpp
@@ -2,19 +2,22 @@
 
 using namespace std;
 
+#define max(a,b) ((a)>(b)?(a):(b))
+/****************************
+        constructor
+*****************************/
 inf_int::inf_int() {
-	length = 1;
-	thesign = true; //consider 0 as positive
-	digits = new char[1]{'0'}; //it's char*
-							   //but it is NOT designed to be used as NULL-terminated string.
-							   //inf_int::length is always same as length of digits
+	*this = inf_int(0);
 }
 
 inf_int::inf_int(int dec){
 	char data[20];
 	if (dec == 0) {
 		thesign = true;
-		digits = new char[1]{'0'};
+		digits = new char[1]{'0'};	//it's char*
+									//but it is NOT designed to be used as NULL-terminated string.
+									//inf_int::length is always same as length of digits
+
 		length = 1;
 	}
 	else if (dec > 0) {
@@ -43,13 +46,103 @@ inf_int::inf_int(int dec){
 		length = i;
 	}
 }
-inf_int::inf_int(const char*) {
-
+inf_int::inf_int(const char* data) {
+	int readfrom = 0;
+	if (strlen(data) == 1 && data[0] == '0') { //data == "0"
+		*this = inf_int();
+		return;
+	}
+	if (data[0] > '0' && data[0] <= '9') { //data is positive
+		thesign = true;
+		length = strlen(data);
+	}
+	else if (data[0] == '+') {
+		thesign = true;
+		readfrom = 1;
+	}
+	else if (data[0] == '-') {
+		thesign = false;
+		readfrom = 1;
+	}
+	else throw -1;
+	data += readfrom; // leave digits only. e.g. ['1','2','3','4','5','\0']
+	length = strlen(data); //e.g. 5
+	digits = new char[length];
+	for (unsigned int i = 0; i < length; i++) {
+		digits[i] = data[length - 1 - i];
+		if (data[length - 1 - i] < '0' || data[length - 1 - i] > '9')
+			throw - 1;
+	}
 }
-inf_int::inf_int(const inf_int&) { // copy constructor
-
+inf_int::inf_int(const inf_int& from) { // copy constructor
+	*this = from;
 }
 inf_int::~inf_int() { delete digits; } // destructor
+
+/******************************
+         operators
+*******************************/
+inf_int& inf_int::operator=(const inf_int& from) {
+	length = from.length;
+	thesign = from.thesign;
+	digits = new char[length];
+	memcpy(digits, from.digits, length);
+	return *this;
+}
+
+bool operator==(const inf_int& a, const inf_int& b) {
+	return (a.thesign == b.thesign && a.length == b.length && strncmp(a.digits, b.digits, max(a.length, b.length)));
+}
+bool operator!=(const inf_int& a, const inf_int& b) {
+	return !(a == b);
+}
+bool operator>(const inf_int& a, const inf_int& b) {
+	if (a.thesign ^ b.thesign) { //sign is different
+		return a.thesign;
+	}
+	else if(a.thesign) { //compare two positive numbers
+		if (a.length != b.length) {
+			return a.length > b.length;
+		}
+		else { //and length are same.
+			for (int i = a.length - 1; i >= 0; i--) {
+				if (a.digits[i] != b.digits[i])
+					return a.digits[i] > b.digits[i];
+			}
+			//both are same number
+			return false;
+		}
+	}
+	else { //compare two negative numbers
+		if (a.length != b.length) {
+			return a.length < b.length;
+		}
+		else {//and length are same
+			for (int i = a.length - 1; i >= 0; i--) {
+				if (a.digits[i] != b.digits[i])
+					return a.digits[i] < b.digits[i];
+			}
+			//both are same number
+			return false;
+		}
+
+	}
+}
+bool operator<(const inf_int& a, const inf_int& b) {
+	return (b > a);
+}
+
+inf_int operator+(const inf_int&, const inf_int&) {
+	return inf_int(0);
+}
+inf_int operator-(const inf_int&, const inf_int&) {
+	return inf_int(0);
+}
+inf_int operator*(const inf_int&, const inf_int&) {
+	return inf_int(0);
+}
+
+
 
 ostream& operator<<(ostream& out, const inf_int& data) {
 	if (data.thesign) {
@@ -64,4 +157,16 @@ ostream& operator<<(ostream& out, const inf_int& data) {
 		}
 	}
 	return out;
+}
+
+istream& operator>>(istream& in, inf_int& data) {
+	string str;
+	in >> str;
+	try {
+		data = inf_int(str.c_str());
+	}
+	catch (int x) { //not an integer. cin.fail() is now true.
+		in.setstate(ios::failbit);
+	}
+	return in;
 }
--- OOP-Project2/main.cpp
+++ OOP-Project2/main.cpp
@@ -9,17 +9,31 @@ using namespace std;
 
 int main()
 {
-	cout << -273 % 10 << endl;
 	inf_int a;
 	inf_int b(100);
 	inf_int c(-273);
 	inf_int d(INT_MAX);
 	inf_int e(INT_MIN);
+	//inf_int f("9321111111111122222822222223333333333NOTANUMBER34444444444455555555559"); //error
+	inf_int f("899999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999998");
+	inf_int g("-899999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999998");
+	inf_int h("+32768");
 	cout << a << endl;
 	cout << b << endl;
 	cout << c << endl;
 	cout << d << endl;
 	cout << e << endl;
+	cout << f << endl;
+	cout << g << endl;
+	cout << h << endl;
+	cout << "input any integer: ";
+	cin >> g;
+	if (cin.fail()) {
+		cout << "seems like it is not a proper integer." << endl;
+	}
+	cout << g << endl;
+	g = b;
+	cout << g << endl;
 	//inf_int c("321111111111122222222222233333333333444444444445555555555");
 	//inf_int d("123451987651234572749499923455022211");
 	//inf_int f = d;
